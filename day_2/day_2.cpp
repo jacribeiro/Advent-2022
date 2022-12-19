181d47ebd0aca373effe788809e6f62a6982e47f
jcribeiro <j.alberto.cribeiro@gmail.com>
2022-12-19 16:38:52 +0000

    Attempt at solution for the first puzzle of day 2

--- day_2/day_2.cpp
+++ day_2/day_2.cpp
@@ -2,9 +2,29 @@
 // Created by johny on 19/12/2022.
 //
 
+#include <vector>
+#include <fstream>
 #include "day_2.h"
 
 int correct_score(string fname) {
-
+    static vector<vector<string>> coding = {{"B X\r", "C Y\r", "A Z\r"},
+                                            {"A X\r", "B Y\r", "C Z\r"},
+                                            {"C X\r", "A Y\r", "B Z\r"}};
+    int score = 0;
+    string line;
+    ifstream file(fname);
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            for (int i = 0; i<3; i++) {
+                for (int j = 0; j<3; j++) {
+                    if (line == coding[i][j]) {
+                        int adding = (i*3) + j + 1;
+                        score += adding;
+                    }
+                }
+            }
+        }
+    }
+    return score;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include "day_1/day_1.h"
+#include "day_2/day_2.h"
 
 using namespace std;
 
 int main() {
-    cout << top3_calories("./day_1/d1_input.csv") << endl;
+    cout << correct_score("./day_2/d2_input.txt") << endl;
 }
